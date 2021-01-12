29031c4710e6436e89747f06646a978c20436a4d
Incredible Tech Solutions <incredibletechsol@gmail.com>
2021-01-12 15:06:12 +0530

    Contact no updated

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
            
             <ul class="contact-details-alt">
                 <li><i class="fa fa-map-marker"></i> <p>Address:A/P-Kalamb-Walchandnagar,Tal : Indapur Dist : Pune,Pin Code:413114</p></li>
-                <li><i class="fa fa-user"></i> <p><strong>Phone</strong>:+91 7066307777 / 7972404918</p></li>
+                <li><i class="fa fa-user"></i> <p><strong>Phone</strong>:+91 7066307777 / 7972404918 / 75174422000</p></li>
                 <li><i class="fa fa-envelope"></i> <p><strong>Email</strong>: <a href="#">pharmacy.kalamb@phadtarepharmacy.org<br>dnyankala20@gmail.com</a></p></li>
             </ul>
         </div>
